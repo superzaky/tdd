cfdddf46315f0a52cfdbe17ffc394bec2a5ebc80
superzaky <superkytoz@gmail.com>
2019-01-03 15:40:22 +0100

    Implement Value Object pattern and tested for that operation.

--- src/main/java/com/my/company/model/Dollar.java
+++ src/main/java/com/my/company/model/Dollar.java
@@ -4,10 +4,15 @@ public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
-        this.amount= amount;
+        this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
--- src/test/java/com/my/company/tdd/AppTest.java
+++ src/test/java/com/my/company/tdd/AppTest.java
@@ -32,7 +32,14 @@ public class AppTest extends TestCase {
      */
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
