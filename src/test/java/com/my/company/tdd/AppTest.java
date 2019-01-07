198a096da5b3baee6693b58be482faa6c1b2e5b8
superzaky <superkytoz@gmail.com>
2019-01-07 16:14:45 +0100

    Decoupling test code from concrete classes by through factory methods.

--- src/test/java/com/my/company/tdd/AppTest.java
+++ src/test/java/com/my/company/tdd/AppTest.java
@@ -2,6 +2,7 @@ package com.my.company.tdd;
 
 import com.my.company.model.Dollar;
 import com.my.company.model.Franc;
+import com.my.company.model.Money;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -32,22 +33,23 @@ public class AppTest extends TestCase {
      * Rigourous Test :-)
      */
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
+    // Below test looks suspicious because it's looks like the test above.
     public void testFrancMultiplication() {
-        Franc five= new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 
     public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-        assertTrue(new Franc(5).equals(new Franc(5)));
-        assertFalse(new Franc(5).equals(new Franc(6)));
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 }
